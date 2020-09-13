0d1020a6cea4d9d8e044e6ff1ece0d04e0b96a4c
eigilak <hello@eigilak.dk>
2020-09-13 14:41:05 +0200

    Done

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import SignUpForm from './components/SignUpForm';
+import firebase from 'firebase/app';
+import LoginForm from "./components/LoginForm";
+import ProfilScreen from "./components/ProfilScreen";
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+    state = {user:null}
+
+    componentWillMount(){
+        const fireBaseConfig ={
+            apiKey: "AIzaSyCSZ7E1KllQBWMjetSbqKmfW7_DdPtPYp4",
+            authDomain: "ovelse5-ec2ee.firebaseapp.com",
+            databaseURL: "https://ovelse5-ec2ee.firebaseio.com/",
+            projectId: "ovelse5-ec2ee",
+            storageBucket: "ovelse5-ec2ee.appspot.com",
+            messagingSenderId: "734463752384",
+            appId: "1:734463752384:web:1fc8dbf164eb8da1965525",
+            measurementId: "G-RNNM8PEH2M"
+        }
+        firebase.initializeApp(fireBaseConfig);
+        firebase
+            .auth()
+            .onAuthStateChanged(user => {
+                console.log('onAuthStateChanged', { U: user });
+
+                this.setState({ user });
+            });
+
+    }
+
+    render() {
+        const {user} = this.state
+        if(!user){
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.paragraph}>
+                        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+                    </Text>
+                    <Card>
+                        <SignUpForm />
+                    </Card>
+                    <Card>
+                        <LoginForm />
+                    </Card>
+                </View>
+            )
+        } else {
+            return (
+
+                <ProfilScreen user={user}/>
+            )
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
